1d224f3853299badb4f0ed936617c51b1f2f0f75
deepak <deepakkumarn086747@gmail.com>
2022-09-07 08:10:16 -0500

    chore: initial project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const router = require('./routes/index');
+
+
+
+app.use(express.json());           //middleware
+app.use(express.urlencoded({extended:false}));
+
+app.use('/api',router);
+
+app.listen(port, () => {
+    console.log(`server successfully started at port ${port}`);
+});
